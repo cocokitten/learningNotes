70975dfc5e2068570dbe490b5f0879c88f9ba3ca
CocoKitten <yangli2309@163.com>
2018-03-16 16:23:07 +0800

    updat

--- C++/virtual_function.cpp
+++ C++/virtual_function.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 using namespace std;
 
+//虚函数用于实现动态多态（一个类函数的调用在运行时期才确定，而非编译期），
+//即允许基类的指针调用子类的函数
+
 class base{
     public:
     virtual void doSomething(){
@@ -18,7 +21,6 @@ class derived:public base
 
 int main(){
     base *p = new derived;
-
-    p->doSomething();
+    p->doSomething();   //I'm derived class
 
 }
